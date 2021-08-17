1af3d249fe78e87529bd56979865cca49b5ad80a
Gamadossantos <milan_total90@hotmail.com>
2021-08-17 12:41:41 -0500

    Updates

---
+++ precache-manifest.6b1c59890b617ebd0ec4e0243999dd47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e462f1d6f08bf0a07e4075e12ccc4c",
+    "url": "/PPT-React/index.html"
+  },
+  {
+    "revision": "3c011a5363430cd5cea3",
+    "url": "/PPT-React/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7427f1acb39f2a1ec5e2",
+    "url": "/PPT-React/static/js/2.c0bd379e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/PPT-React/static/js/2.c0bd379e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c011a5363430cd5cea3",
+    "url": "/PPT-React/static/js/main.3d614229.chunk.js"
+  },
+  {
+    "revision": "bb67735495253810e051",
+    "url": "/PPT-React/static/js/runtime-main.730d97d5.js"
+  }
+]);
